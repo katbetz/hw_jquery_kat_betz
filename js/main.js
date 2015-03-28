b854595a82ea2fe1689dcb664306c337ae2f7c80
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-28 00:39:06 -0400

    indent appropriately

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-var paragraphs = $("p");
-var pobject ={
-    fontSize: "1.1",
-    color: "indigo",
-    padding: "5px"
-}
-$(paragraphs).css(pobject);
+    var paragraphs = $("p");
+    var pobject ={
+        fontSize: "1.1",
+        color: "indigo",
+        padding: "5px"
+    }
+    $(paragraphs).css(pobject);
 
 });
